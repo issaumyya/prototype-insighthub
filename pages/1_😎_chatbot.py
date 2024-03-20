1f446eb82475bce397059b8a342dfc49c05a37d6
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-20 20:52:54 +0530

    Update 1_😎_chatbot.py

--- pages/1_😎_chatbot.py
+++ pages/1_😎_chatbot.py
@@ -11,12 +11,12 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-with st.chat_message("assistant"):
+    # Display assitant message in chat message container
+    with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
         # Simulate stream of response with milliseconds delay
@@ -29,11 +29,10 @@ with st.chat_message("assistant"):
             #will provide lively writing
             stream=True,
         ):
-
-#get content in response
+            #get content in response
             full_response += response.choices[0].delta.get("content", "")
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
- # Add assistant response to chat history
+    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
