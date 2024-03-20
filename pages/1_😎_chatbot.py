3efd949d0d9a0f15bf02fc07cf183d7b175abe29
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-20 22:24:12 +0530

    Update 1_😎_chatbot.py

--- pages/1_😎_chatbot.py
+++ pages/1_😎_chatbot.py
@@ -1,66 +1,31 @@
-import streamlit as st
-from openai import OpenAI
-
-st.title("ChatGPT-powered ChatBot")
-with st.expander("ℹ️ Disclaimer"):
-    st.caption(
-        """We appreciate your engagement! Please note, this demo is designed to
-        process a maximum of 10 interactions and may be unavailable if too many
-        people use the service concurrently. Thank you for your understanding.
-        """
-    )
-
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-if "max_messages" not in st.session_state:
-    # Counting both user and assistant messages, so 10 rounds of conversation
-    st.session_state.max_messages = 50
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if len(st.session_state.messages) >= st.session_state.max_messages:
-    st.info(
-        """Notice: The maximum message limit for this demo version has been reached. We value your interest!
-        We encourage you to experience further interactions by building your own application with instructions
-        from Streamlit's [Build a basic LLM chat app](https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps)
-        tutorial. Thank you for your understanding."""
-    )
-
-else:
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        with st.chat_message("assistant"):
-            try:
-                stream = client.chat.completions.create(
-                    model=st.session_state["openai_model"],
-                    messages=[
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ],
-                    stream=True,
-                )
-                response = st.write_stream(stream)
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": response}
-                )
-            except:
-                st.session_state.max_messages = len(st.session_state.messages)
-                rate_limit_message = """
-                    Oops! Sorry, I can't talk now. Too many people have used
-                    this service recently.
-                """
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": rate_limit_message}
-                )
-                st.rerun()
+import openai
+import streamlit
+st.title("ChatBot")
+#connect openai key
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assitant message in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        # Simulate stream of response with milliseconds delay
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            #will provide lively writing
+            stream=True,
+        ):
+            #get content in response
+            full_response += response.choices[0].delta.get("content", "")
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
