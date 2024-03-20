b40f7cc9c55b82039bb8f6dcac56238ac1713b26
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-20 23:11:34 +0530

    Update 1_😎_chatbot.py

--- pages/1_😎_chatbot.py
+++ pages/1_😎_chatbot.py
@@ -31,10 +31,5 @@ if prompt := st.chat_input("What is up?"):
             stream=True,
         )
         response = st.write_stream(stream)
-        #get content in response
-        full_response += response.choices[0].delta.get("content", "")
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
         # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": full_response})
