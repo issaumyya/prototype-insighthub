2ba13ed71f63fdfa28e863cf154758a674b0d82b
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-20 21:02:06 +0530

    Update 1_😎_chatbot.py

--- pages/1_😎_chatbot.py
+++ pages/1_😎_chatbot.py
@@ -2,7 +2,8 @@ import openai
 import streamlit as st
 st.title("ChatBot")
 #connect openai key
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+openai.api_key = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 if "messages" not in st.session_state:
