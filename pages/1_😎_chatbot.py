cbb9d7a163aafc65c1e5f81a8e9610781b7edbc6
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-21 09:56:38 +0530

    Update 1_😎_chatbot.py

--- pages/1_😎_chatbot.py
+++ pages/1_😎_chatbot.py
@@ -6,33 +6,31 @@ st.title("ChatBot")
 
 # Connect OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-# Initialize openai_model if not present in session state
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
-
 # Initialize messages attribute if it doesn't exist
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Handle user input and display chat messages
-if prompt := st.text_input("You:"):
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-
     # Display user message in chat message container
-    st.write("User:", prompt)
-
-    # Display assistant message in chat message container
-    full_response = ""
-    for response in openai.ChatCompletion.create(
-        model=st.session_state["openai_model"],
-        messages=st.session_state.messages,
-        # Will provide lively writing
-        stream=True,
-    ):
-        full_response += response.choices[0].text.strip()
-        # Display assistant message in chat message container
-        st.write("Assistant:", full_response)
-
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assitant message in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        # Simulate stream of response with milliseconds delay
+        stream = openai.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            #will provide lively writing
+            stream=True,
+        )
+        response = st.write_stream(stream)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response}) 
