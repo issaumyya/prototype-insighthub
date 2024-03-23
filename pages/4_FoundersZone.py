d6636de82d7ec55ac462cd7afa81ada0fa37f502
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-23 19:59:11 +0530

    Update 4_FoundersZone.py

--- pages/4_FoundersZone.py
+++ pages/4_FoundersZone.py
@@ -11,17 +11,17 @@ col1, col2, col3 = st.columns([3, 1, 1])
 # Create a container within the first column
 with col1: 
   st.title("Indian Startups at a Glance")
+  st.sidebar.header("Filter Options")
+  top_states_slider = st.slider("Number of Top States", min_value=1, max_value=10, value=5)
+  filter_by_select = st.selectbox(
+      "Filter By", ["Startups", "Accelerators", "Incubators"])
   # Define a function to create the graph with filtering options
   def create_state_distribution(data, filter_by, top_value):
     filtered_data = data.nlargest(top_value, filter_by)  # Filter top states
     fig = px.bar(filtered_data, x="State", y=filter_by, title=f"Top {top_value} States by {filter_by.capitalize()}")
     return fig
-  st.sidebar.header("Filter Options")
-  top_states_slider = st.sidebar.slider("Number of Top States", min_value=1, max_value=10, value=5)
-  filter_by_select = st.sidebar.selectbox(
-      "Filter By", ["Startups", "55Accelerators", "Incubators"])
   startup_graph = create_state_distribution(data.copy(), "Startups", top_states_slider)
-  accelerator_graph = create_state_distribution(data.copy(), "55Accelerators", top_states_slider)
+  accelerator_graph = create_state_distribution(data.copy(), "Accelerators", top_states_slider)
   incubator_graph = create_state_distribution(data.copy(), "Incubators", top_states_slider)
     
   st.header("Distribution of Startups Across States")
