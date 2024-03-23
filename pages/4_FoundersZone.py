e5ace022ae7f28005ed80e77f8b8d45b8969f45b
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-23 19:50:37 +0530

    Update 4_FoundersZone.py

--- pages/4_FoundersZone.py
+++ pages/4_FoundersZone.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from plotly.express import px
+import plotly.graph_objects as go
 
 data = pd.read_csv("StartupIndiaStats.csv")
 
@@ -10,12 +11,7 @@ col1, col2, col3 = st.columns([3, 1, 1])
 # Create a container within the first column
 with col1:
   st.title("Indian Startups at a Glance")
-  nested_container = st.container()
-
-# Create nested columns inside the container (visually appears within col1)
-nested_col1, nested_col2, nested_col3 = nested_container.columns(3)
-with nested_col1:
-    fig_state_distribution = px.bar(
+  fig_state_distribution = px.bar(
     data, x="State", y="Startups", title="Distribution of Startups Across States")
     st.plotly_chart(fig_state_distribution)
 
@@ -25,12 +21,3 @@ with col2:
 
 with col3:
   st.write("Content in Column 3")
-
-with nested_col1:
-  st.write("Nested Content 1")
-
-with nested_col2:
-  st.write("Nested Content 2")
-
-with nested_col3:
-  st.write("Nested Content 3")
