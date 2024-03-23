9f5713a4836160cdb59a7d64a860e60d360e72f2
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-23 19:43:31 +0530

    Update 4_FoundersZone.py

--- pages/4_FoundersZone.py
+++ pages/4_FoundersZone.py
@@ -1,25 +1,16 @@
 import streamlit as st
-border_style = """
-<style>
-  .stContainer[data-testid="stColumn"][data-index="0"] {
-    border: 2px solid #007bff;
-    padding: 10px;
-    border-radius: 5px;
-  }
-</style>
-"""
-
-# Display the CSS styles
-st.markdown(border_style, unsafe_allow_html=True)
 # Create initial columns with desired ratio (3:1:1)
 col1, col2, col3 = st.columns([3, 1, 1])
 
 # Create a container within the first column
 with col1:
+  st.title("Indian Startups at a Glance")
   nested_container = st.container()
 
 # Create nested columns inside the container (visually appears within col1)
 nested_col1, nested_col2, nested_col3 = nested_container.columns(3)
+with nested_col1:
+  
 
 # Add content to columns and nested columns
 with col2:
