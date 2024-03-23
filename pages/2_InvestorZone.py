7bd03ee48c4e72467c8e363a289b13205bbbf8e4
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-24 01:22:34 +0530

    Update 2_InvestorZone.py

--- pages/2_InvestorZone.py
+++ pages/2_InvestorZone.py
@@ -25,9 +25,9 @@ if not pd.api.types.is_numeric_dtype(df1["Funding_num"]):
 df1.dropna(subset=["Funding_num"])                                       
 df1["Funding Stage"] = pd.cut(df1["Funding_num"], bins=[0, 10, 50, 100, float("inf")], labels=["Seed", "Series A", "Series B+", "Late Stage"])
 sector_filter = st.sidebar.selectbox("Filter by Sector", df1["Sector"].unique())
-funding_range = st.sidebar.slider("Total Funding Range", min_value=0, max_value=df1["Funding_num"].max(), value=df1["Funding_num"].median())
-min_years_unicorn = st.sidebar.slider("Minimum Years to Unicorn", min_value=0, max_value=df1.get("Years to Unicorn", 0).max(), value = df1.get("Years to Unicorn", 0).median())
-min_valuation = st.sidebar.slider("Minimum Valuation (USD Billion)", min_value=0, max_value=df1["Val_num"].max(), value = df1["Val_num"].median())
+funding_range = st.slider("Total Funding Range", min_value=0, max_value=df1["Funding_num"].max())
+min_years_unicorn = st.slider("Minimum Years to Unicorn", min_value=0, max_value=df1.get("Years to Unicorn", 0).max())
+min_valuation = st.slider("Minimum Valuation (USD Billion)", min_value=0, max_value=df1["Val_num"].max())
 stage_filter = st.sidebar.selectbox("Funding Stage", df1["Funding Stage"].unique())
 
 filtered_data = df1[
