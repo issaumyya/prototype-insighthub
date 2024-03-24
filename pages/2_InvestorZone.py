b3fc59b648e34e693bcf023e8a42fe195059b5f6
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-24 10:15:58 +0530

    Update 2_InvestorZone.py

--- pages/2_InvestorZone.py
+++ pages/2_InvestorZone.py
@@ -4,23 +4,30 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-years = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
-funding_amount_raised = [5.4, 9.3, 5.6, 13, 11.9, 12, 12, 42, 25]
-no_of_deals = [378, 985, 1049, 999, 833, 810, 953, 1584, 1519]
+years = ["Q1-2020", "Q2-2020", "Q3-2020", "Q4-2020", "Q1-2021", "Q2-2021", "Q3-2021", "Q3-2021", "Q1-2022",
+        "Q2-2022", "Q3-2022", "Q4-2022","Q1-2023", "Q2-2023", "Q3-2023", "Q4-2023 (Till Nov.)"]
+funding_amount_raised = [4, 1, 3, 4, 4, 6, 17, 15, 12, 7, 3, 3, 2.9, 2.5, 1.7, 1.6]
+no_of_deals = [212, 179, 278, 284, 308, 313, 465, 498, 506, 394, 334, 283, 229, 241, 221, 138]
+
+data = pd.DataFrame({"Timeline": years, "Funding Amount": funding_amount_raised, "Deal Count": no_of_deals})
+
+# Filter year based on Streamlit sidebar selection
+selected_year = st.sidebar.selectbox("Timeline", data["Timeline"].unique())
+filtered_data = data[data["Timeline"] == selected_year]
 
 # Create a figure with two y-axes
-fig = go.FigureWidget(make_subplots(specs=[[{"secondary_y": True}]]))
+fig = go.Figure(make_subplots(specs=[[{"secondary_y": True}]]))
 
 # Defining the two y-axes
 fig.add_trace(go.Bar(
-    x=years,
-    y=funding_amount_raised,
+    x=filtered_data["Timeline"],
+    y=filtered_data["Funding Amount"],
     name="Funding Amount ($ Bn)",
 ))
 
 fig.add_trace(go.Scatter(
-    x=years,
-    y=no_of_deals,
+    x=filtered_data["Timeline"],
+    y=filtered_data["Deal Count"],
     name="Deal Count",
     mode="lines",
     yaxis="y2"
@@ -28,7 +35,7 @@ fig.add_trace(go.Scatter(
 
 fig.update_layout(
     title="Double Axis Graph (Bar & Line)",
-    xaxis_title="Years",
+    xaxis_title="Timeline",
     yaxis_title="Funding Amount ($ Bn)",
     yaxis2_title="Deal Count",
 )
