f7f82a256010ae823ff0c2dd9ad5abe0c2096f06
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-24 10:21:14 +0530

    Update 2_InvestorZone.py

--- pages/2_InvestorZone.py
+++ pages/2_InvestorZone.py
@@ -34,6 +34,10 @@ fig.update_layout(
     xaxis_title="Timeline",
     yaxis_title="Funding Amount ($ Bn)",
     yaxis2_title="Deal Count",
+    xaxis_showgrid=False,  # Remove x-axis grid lines
+    yaxis_showgrid=False,  # Remove y-axis grid lines
+    xaxis_zeroline=False,  # Remove zero line for x-axis
+    yaxis_zeroline=False
 )
 
 fig.update_layout(xaxis=dict(tickmode="linear"), bargap=0.1)
