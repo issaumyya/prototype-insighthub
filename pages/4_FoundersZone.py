477bf1c2a56df699eb7276447be463706fe807fd
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-24 18:04:43 +0530

    Update 4_FoundersZone.py

--- pages/4_FoundersZone.py
+++ pages/4_FoundersZone.py
@@ -18,9 +18,6 @@ with col2:
             background: rgba(255, 255, 255, 0.1);
             padding: 2px;
             border-radius: 15px;
-            box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-            backdrop-filter: blur(8px);
-            -webkit-backdrop-filter: blur(8px);
             border: 1px solid rgba(255, 255, 255, 0.18);
             color: #ffffff;
             text-align: center;
