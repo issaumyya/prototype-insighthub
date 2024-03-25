dd04fbc9260cce37af608e2c381a22479d22f205
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-25 10:35:32 +0530

    Update 4_FoundersZone.py

--- pages/4_FoundersZone.py
+++ pages/4_FoundersZone.py
@@ -28,7 +28,31 @@ col1, col2 = st.columns((2))
 with col1:
   top_states_slider = st.slider("Number of Top States", min_value=1, max_value=10, value=5)
 with col2:
-  filter_by_select = st.selectbox("Filter By", ["Startups", "Accelerators", "Incubators"])
+     filter_by_select = st.selectbox("Filter By", ["Startups", "Accelerators", "Incubators"])
+    
+def create_state_distribution(data, filter_by, top_value):
+  filtered_data = data.nlargest(top_value, filter_by)  # Filter top states
+  fig = px.bar(filtered_data, x="State", y=filter_by, title=f"Top {top_value} States by {filter_by.capitalize()}")
+  #fig.update_traces(marker_color='gray')  # Set initial color
+  max_value = filtered_data[filter_by].max()
+  fig.update_yaxes(range=[0, max_value * 1.2])
+  return fig
+selected_filter = filter_by_select
+if selected_filter == "Startups":
+    startup_graph = create_state_distribution(data.copy(), "Startups", top_states_slider)
+    st.plotly_chart(startup_graph,use_container_width=True, height = 150)
+elif selected_filter == "Accelerators":
+    accelerator_graph = create_state_distribution(data.copy(), "Accelerators", top_states_slider)
+    st.plotly_chart(accelerator_graph,use_container_width=True, height = 150)
+elif selected_filter == "Incubators":
+    incubator_graph = create_state_distribution(data.copy(), "Incubators", top_states_slider)
+    st.plotly_chart(incubator_graph,use_container_width=True, height = 150)
+else:
+    st.write("Please select a filter from the sidebar.")
+st.markdown(textbox_style, unsafe_allow_html=True)
+st.markdown(f"<div class='textbox'><h3>✅ Revenue Growth is the key metric to focus for early stage startups<h3></div>", unsafe_allow_html=True)
+
+with col1:
   sectors = ["Fintech", "Ecommerce", "Enterprisetech", "Media & Entertainment", "Edtech", "Traveltech", "Others"]
   soonicorns = [37, 18, 17, 7, 7, 7, 22]
   df2 = pd.DataFrame({"Sectors":sectors, "Soonicorns":soonicorns})
@@ -43,34 +67,13 @@ with col2:
   st.plotly_chart(pie_fig,use_container_width=True, height = 120)
   st.markdown(textbox_style, unsafe_allow_html=True)
   st.markdown(f"<div class='textbox'><h3>✅ Fintech is the most likely sector to generate future unicorns in India<h3></div>", unsafe_allow_html=True)
-def create_state_distribution(data, filter_by, top_value):
-  filtered_data = data.nlargest(top_value, filter_by)  # Filter top states
-  fig = px.bar(filtered_data, x="State", y=filter_by, title=f"Top {top_value} States by {filter_by.capitalize()}")
-  #fig.update_traces(marker_color='gray')  # Set initial color
-  max_value = filtered_data[filter_by].max()
-  fig.update_yaxes(range=[0, max_value * 1.2])
-  return fig
-with col1: 
-  selected_filter = filter_by_select
-  if selected_filter == "Startups":
-    startup_graph = create_state_distribution(data.copy(), "Startups", top_states_slider)
-    st.plotly_chart(startup_graph,use_container_width=True, height = 150)
-  elif selected_filter == "Accelerators":
-    accelerator_graph = create_state_distribution(data.copy(), "Accelerators", top_states_slider)
-    st.plotly_chart(accelerator_graph,use_container_width=True, height = 150)
-  elif selected_filter == "Incubators":
-    incubator_graph = create_state_distribution(data.copy(), "Incubators", top_states_slider)
-    st.plotly_chart(incubator_graph,use_container_width=True, height = 150)
-  else:
-    st.write("Please select a filter from the sidebar.")
-  st.markdown(textbox_style, unsafe_allow_html=True)
-  st.markdown(f"<div class='textbox'><h3>✅ Revenue Growth is the key metric to focus for early stage startups<h3></div>", unsafe_allow_html=True)
-
-sectors = ["Upto 2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
-no_of_unicorns = [4,3,3,2,1,10,7,12,45,22,2]
-uni_fig = px.bar(x=sectors, y=no_of_unicorns)
-st.plotly_chart(uni_fig,use_container_width=True, height = 150)
-st.text("Only 2 Unicorns were Minted in 2023, a decline by 91% from last year")
+with col2:
+    sectors = ["Upto 2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
+    no_of_unicorns = [4,3,3,2,1,10,7,12,45,22,2]
+    uni_fig = px.bar(x=sectors, y=no_of_unicorns)
+    st.plotly_chart(uni_fig,use_container_width=True, height = 150)
+    st.markdown(textbox_style, unsafe_allow_html=True)
+    st.markdown(f"<div class='textbox'><h3>✅ Only 2 Unicorns were Minted in 2023, a decline by 91% from last year<h3></div>", unsafe_allow_html=True)
 
 import plotly.graph_objects as go
 
@@ -87,37 +90,33 @@ fig2.update_layout(
 )
 st.plotly_chart(fig2,use_container_width=True, height = 150)
 
-#treemap
-data1 = {
-    'Sector': [
-        'Fintech', 'Fintech', 'Fintech', 'Fintech', 'Fintech', 'Fintech',
-        'Ecommerce', 'Ecommerce', 'Ecommerce', 'Ecommerce',
-        'Edtech', 'Edtech', 'Edtech', 'Edtech', 'Edtech', 'Edtech',
-        'Deeptech', 'Deeptech', 'Deeptech', 'Deeptech', 'Deeptech'
-    ],
-    'Subsector': [
-        'Lending Tech', 'Banking', 'Fintech SaaS', 'Insurtech', 'Investment Tech', 'Others',
-        'D2C', 'B2B', 'C2B', 'Others',
-        'Skill Development', 'Test Prep', 'Online Discovery', 'Online Certification', 'Edtech SaaS', 'Others',
-        'RPA', 'Spacetech', 'Hardware & IoT', 'Dronetech', 'Others'
-    ],
-    'Value': [
-        1185, 971, 348, 314, 107, 96,
-        1415, 448, 682, 91,
-        72, 64, 55, 45, 43, 5,
-        147, 124, 123, 29, 9
-    ]
-}
-
-df4 = pd.DataFrame(data1)
-fig3 = px.treemap(
-  df4,  # Data source (DataFrame)
-  path=[df4['Sector'], df4['Subsector']],  # Path for hierarchical levels (Sector, Subsector)
-  values='Value',  # Values for size representation
-  color='Sector',  # Optional color by sector (categorical coloring)
-  title='Funding Amount (in Mn) by Sector'  # Treemap title
-)
-
-# Display the treemap in Streamlit
-st.plotly_chart(fig3)
+with col1:
+    data1 = {
+        'Sector': [
+            'Fintech', 'Fintech', 'Fintech', 'Fintech', 'Fintech', 'Fintech',
+            'Ecommerce', 'Ecommerce', 'Ecommerce', 'Ecommerce',
+            'Edtech', 'Edtech', 'Edtech', 'Edtech', 'Edtech', 'Edtech',
+            'Deeptech', 'Deeptech', 'Deeptech', 'Deeptech', 'Deeptech'
+        ],
+        'Subsector': [
+            'Lending Tech', 'Banking', 'Fintech SaaS', 'Insurtech', 'Investment Tech', 'Others',
+            'D2C', 'B2B', 'B2C', 'Others',
+            'Skill Development', 'Test Prep', 'Online Discovery', 'Online Certification', 'Edtech SaaS', 'Others',
+            'RPA', 'Spacetech', 'Hardware & IoT', 'Dronetech', 'Others'
+        ],
+        'Value': [
+            1185, 971, 348, 314, 107, 96,
+            1415, 448, 682, 91,
+            72, 64, 55, 45, 43, 5,
+            147, 124, 123, 29, 9
+        ]
+    }
+    df4 = pd.DataFrame(data1)
+    fig3 = px.treemap(
+        df4,
+        path=[df4['Sector'], df4['Subsector']],  # Path for hierarchical levels (Sector, Subsector)
+        values='Value',  
+        color='Sector', 
+        title='Funding Amount (in Mn) by Sector'  # Treemap title)
+    st.plotly_chart(fig3)
 
