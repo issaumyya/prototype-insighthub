ff2280b23d19dc0341e38ab99141a5ddc6bc20cc
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-27 11:27:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ textbox_style = """
 sectors = ["Upto 2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 no_of_unicorns = [4,3,3,2,1,10,7,12,45,22,2]
 uni_fig = px.bar(x=sectors, y=no_of_unicorns)
-with col1:
+with col2:
     st.plotly_chart(uni_fig,use_container_width=True, height = 150)
     st.markdown(textbox_style, unsafe_allow_html=True)
     st.markdown(f"<div class='textbox'><h1>2</h1> <h3>Unicorns were Minted in 2023, a decline by 91% from last year<h3></div>", unsafe_allow_html=True)
