2fae4b887504d0b27303311de71b553cb414f569
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-29 13:18:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,7 @@ textbox_style = """
         }
     </style>
 """
-with st.sidebar:
-    with st.popover("Open popover"):
-        name = st.text_input("What's your name?")
-    st.text(name, "good to see you!")
+
 st.header('I N S I G H T  H U B', divider='blue')
 st.subheader('_Empowering_ enterpreneur landscape of :blue[India] :sunglasses:')
 
