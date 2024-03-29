0f426ef7540d2ac13d74ac1ef1cccd07e616e0b3
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-29 13:40:05 +0530

    Update 2_InvestorZone.py

--- pages/2_InvestorZone.py
+++ pages/2_InvestorZone.py
@@ -58,9 +58,11 @@ fig.update_layout(legend=dict(orientation="h", x=0, y=1.5))
 with col1:
         st.plotly_chart(fig,use_container_width=True, height = 170)
         col11, col12 = st.columns((2))
-        with col11:
-                st.markdown(textbox_style, unsafe_allow_html=True)
-                st.markdown(f"<div class='textbox'><h1>35%</h1><h3> YoY Decline in median ticket size of funding</h3></div>", unsafe_allow_html=True)
+        col11.metric("YoY Decline in median ticket size of funding", "$437.8", "-35%")
+                
+                #st.markdown(textbox_style, unsafe_allow_html=True)
+                #st.markdown(f"<div class='textbox'><h1>35%</h1><h3> YoY Decline in median ticket size of funding</h3></div>", unsafe_allow_html=True)
+                
         with col12:
                 st.markdown(textbox_style, unsafe_allow_html=True)
                 st.markdown(f"<div class='textbox'><h1>$9 Bn </h1><h3>Total Funding in 2023</h3></div>", unsafe_allow_html=True)
