4dceb20995dbece27b61fd1992495a570b1c3510
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-29 16:30:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,35 @@ with col3:
 
 tab1, tab2, tab3 = st.tabs(["India                 ", "Global                 ", "About Us                 "])
 with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.header("Latest News")
+    import streamlit as st
+    business_emoji = "📊"
+    government_emoji = "️🚨"
+    startup_emoji = "💰"
+    investment_emoji = "💸"
+    technology_emoji = "🖥"
+
+    news = {
+        business_emoji: "RateGain Founder Bhanu Chopra’s Family Members Offload 3% Stake",
+        startup_emoji: "Startup Mahakumbh: Startups Supported By MeitY Startup Hub Showcase Their Innovations",
+        business_emoji: "BillDesk’s FY23 Profit Dips 5%, Revenue Inches Closer To INR 3,000 Cr Mark",
+        investment_emoji: "Sustainable Packaging Startup Bambrew Bags INR 60 Cr From Blume Ventures, Others",
+        government_emoji: "There Are Chances Of Misuse Of AI Without Proper Training: PM Narendra Modi",
+        business_emoji: "Keep Proceeds From Rights Issues In Escrow Account, Consider Extension Of Issue: NCLT To BYJU’S",
+        government_emoji: "Startups Under I-T Dept Scanner, Get Tax Notices Over VC Funding",
+        business_emoji: "Swiggy’s Reported $207 Mn In Losses Between Q1 & Q3 FY24",
+        investment_emoji: "TAC Infosec IPO Day 2: Public Issue Oversubscribed 22.8X",
+        business_emoji: "Mamaearth Forays Into Colour Cosmetics Space With ‘Staze’",
+        investment_emoji: "L Catterton To Launch India-Focussed Consumer Fund",
+        business_emoji: "PhonePe Enables UPI Payments For Indians In UAE",
+    }
+
+    for emoji, title in news.items():
+        st.subheader(f"{emoji} {title}")
+
 with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.header("Latest News")
+
 with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.header("About InsightHub")
+
