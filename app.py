fe53a9a7c0a864748f8333d6ea70e54cb9b6b737
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-03-29 16:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,16 @@ with col2:
     st.plotly_chart(uni_fig,use_container_width=True, height = 150)
     st.markdown(textbox_style, unsafe_allow_html=True)
     st.markdown(f"<div class='textbox'><h1>2</h1> <h3>Unicorns were Minted in 2023, a decline by 91% from last year<h3></div>", unsafe_allow_html=True)
-with col1:
+    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+    with tab1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    with tab2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    with tab3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     if st.button("Fellow Investor?"):
         st.switch_page("pages/2_InvestorZone.py")
     if st.button("Fellow Founder?"):
